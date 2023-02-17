a89070c8879f4a247b0f5df2db9430acea1762cc
The Gerro <Kylevanderross@gmail.com>
2023-02-17 15:37:10 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 /**
  * main - Entry point
- * Description: Write a program that prints all possible different combinations of two digits
  *
  * Return: Always 0 (Success)
  */
@@ -27,7 +26,6 @@ int main(void)
 					putchar(' ');
 				}
 			}
-			
 			c++;
 		}
 		d++;

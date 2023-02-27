5d38628cee984e4a21772e0845e171564daa8bd1
The Gerro <Kylevanderross@gmail.com>
2023-02-27 09:43:45 -0800

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,27 +1,25 @@
 #include "main.h"
 
 /**
- * leet - Encodes a string to 1337
- * @str: The string to be encoded
- *
- * Return: A pointer to the encoded string
+ * puts_half - Prints half of a string
+ * @str: The string to print
+ * Return: void
  */
-char *leet(char *str)
+
+void puts_half(char *str)
+
 {
-	int indx1 = 0, indx2;
-	char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
 
-	while (str[indx1])
+	int j = 0;
+	int k;
+
+	while (str[j] != '\0')
 	{
-		for (indx2 = 0; indx2 <= 7; indx2++)
-		{
-			if (str[indx1] == leet[indx2] ||
-				str[indx1] - 32 == leet[indx2])
-				str[indx1] = indx2 = '0';
-		}
-
-		indx1++;
+	j++;
 	}
-
-	return (str);
+	if (j % 2 == 1)
+	{
+	k = ((j - 1) / 2;
+	k += 1;
+	}
 }

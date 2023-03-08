60626998ea2669b6fdd46a53a5fd7d37d7180078
The Gerro <Kylevanderross@gmail.com>
2023-03-08 08:52:30 -0800

    6-is_prime_number.c

--- 0x08-recursion/100-main.c
+++ 0x08-recursion/100-main.c
@@ -12,11 +12,15 @@ int main(void)
 
 	r = is_palindrome("level");
 	printf("%d\n", r);
+
 	r = is_palindrome("redder");
 	printf("%d\n", r);
+
 	r = is_palindrome("test");
 	printf("%d\n", r);
+
 	r = is_palindrome("step on no pets");
 	printf("%d\n", r);
+
 	return (0);
 }

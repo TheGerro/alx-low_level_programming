61b8ca490eeb54bc50aff19f9895d8a5f035ee12
The Gerro <Kylevanderross@gmail.com>
2023-03-13 06:13:06 -0700

    Static libraries

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -9,10 +9,8 @@
   */
 
 char *_strcat(char *dest, char *src)
-
 {
 	int i;
-
 	int j;
 
 	i = 0;
@@ -21,19 +19,14 @@ char *_strcat(char *dest, char *src)
 	{
 		i++;
 	}
-
 	j = 0;
-
 	while (src[j] != '\0')
 	{
 		dest[i] = src[j];
-
 		i++;
-
 		j++;
 	}
 
 	dest[i] = '\0';
-
 	return (dest);
 }
